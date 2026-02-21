03666446ac7f2f3b85c0644180d25b74a98dc917
ppg94 <2730409520@qq.com>
2026-02-21 23:43:54 +0800

    feat: select query images with highest weight controversy, update questions 1-10

--- app.js
+++ app.js
@@ -29,113 +29,113 @@ function showPage(pageState) {
 
 const QUESTIONS = [
   {
-    id: 6,
-    queryImage: '题目6/题目图片/video26_000166.jpg',
+    id: 1,
+    queryImage: '题目1/题目图片/video072_000102.jpg',
     options: [
-      '题目6/选项图片/video26_000216.jpg',
-      '题目6/选项图片/video26_000330.jpg',
-      '题目6/选项图片/video26_000380.jpg',
-      '题目6/选项图片/video26_000452.jpg',
-      '题目6/选项图片/video26_000474.jpg'
+      '题目1/选项图片/video87_000498.jpg',
+      '题目1/选项图片/video067_000210.jpg',
+      '题目1/选项图片/video072_000110.jpg',
+      '题目1/选项图片/video072_000119.jpg',
+      '题目1/选项图片/video072_000127.jpg',
+    ]
+  },
+  {
+    id: 2,
+    queryImage: '题目2/题目图片/video86_000989.jpg',
+    options: [
+      '题目2/选项图片/video86_000984.jpg',
+      '题目2/选项图片/video071_000295.jpg',
+      '题目2/选项图片/video86_001018.jpg',
+      '题目2/选项图片/video86_001017.jpg',
+      '题目2/选项图片/video86_001019.jpg',
+    ]
+  },
+  {
+    id: 3,
+    queryImage: '题目3/题目图片/video108_001386.jpg',
+    options: [
+      '题目3/选项图片/video57_000195.jpg',
+      '题目3/选项图片/video086_000429.jpg',
+      '题目3/选项图片/video59_000073.jpg',
+      '题目3/选项图片/video59_000096.jpg',
+      '题目3/选项图片/video59_000703.jpg',
+    ]
+  },
+  {
+    id: 4,
+    queryImage: '题目4/题目图片/video086_000610.jpg',
+    options: [
+      '题目4/选项图片/video088_000121.jpg',
+      '题目4/选项图片/video075_000539.jpg',
+      '题目4/选项图片/video58_000117.jpg',
+      '题目4/选项图片/video088_000176.jpg',
+      '题目4/选项图片/video58_000480.jpg',
+    ]
+  },
+  {
+    id: 5,
+    queryImage: '题目5/题目图片/video108_000842.jpg',
+    options: [
+      '题目5/选项图片/video108_000832.jpg',
+      '题目5/选项图片/video62_000238.jpg',
+      '题目5/选项图片/video108_000843.jpg',
+      '题目5/选项图片/video62_000350.jpg',
+      '题目5/选项图片/video26_000401.jpg',
+    ]
+  },
+  {
+    id: 6,
+    queryImage: '题目6/题目图片/video070_000858.jpg',
+    options: [
+      '题目6/选项图片/video070_000579.jpg',
+      '题目6/选项图片/video070_000818.jpg',
+      '题目6/选项图片/video70_001434.jpg',
+      '题目6/选项图片/video71_000689.jpg',
+      '题目6/选项图片/video66_000061.jpg',
     ]
   },
   {
     id: 7,
-    queryImage: '题目7/题目图片/video58_001569.jpg',
+    queryImage: '题目7/题目图片/video74_000404.jpg',
     options: [
-      '题目7/选项图片/video58_001542.jpg',
-      '题目7/选项图片/video59_000104.jpg',
-      '题目7/选项图片/video59_000227.jpg',
-      '题目7/选项图片/video59_000265.jpg',
-      '题目7/选项图片/video59_000478.jpg'
+      '题目7/选项图片/video70_001109.jpg',
+      '题目7/选项图片/video57_000554.jpg',
+      '题目7/选项图片/video74_000504.jpg',
+      '题目7/选项图片/video74_000388.jpg',
+      '题目7/选项图片/video74_000385.jpg',
     ]
   },
   {
     id: 8,
-    queryImage: '题目8/题目图片/video26_000592.jpg',
+    queryImage: '题目8/题目图片/video60_000639.jpg',
     options: [
-      '题目8/选项图片/video26_000772.jpg',
-      '题目8/选项图片/video26_000776.jpg',
-      '题目8/选项图片/video57_000075.jpg',
-      '题目8/选项图片/video57_000175.jpg',
-      '题目8/选项图片/video57_000644.jpg'
+      '题目8/选项图片/video58_000327.jpg',
+      '题目8/选项图片/video082_000171.jpg',
+      '题目8/选项图片/video60_000637.jpg',
+      '题目8/选项图片/video60_000638.jpg',
+      '题目8/选项图片/video60_000636.jpg',
     ]
   },
   {
     id: 9,
-    queryImage: '题目9/题目图片/video62_000323.jpg',
+    queryImage: '题目9/题目图片/video108_000239.jpg',
     options: [
-      '题目9/选项图片/video62_000267.jpg',
-      '题目9/选项图片/video62_000325.jpg',
-      '题目9/选项图片/video62_000350.jpg',
-      '题目9/选项图片/video62_000388.jpg',
-      '题目9/选项图片/video62_000594.jpg'
+      '题目9/选项图片/video108_000348.jpg',
+      '题目9/选项图片/video071_000151.jpg',
+      '题目9/选项图片/video108_000127.jpg',
+      '题目9/选项图片/video108_000189.jpg',
+      '题目9/选项图片/video26_000657.jpg',
     ]
   },
   {
     id: 10,
-    queryImage: '题目10/题目图片/video62_000589.jpg',
+    queryImage: '题目10/题目图片/video58_000480.jpg',
     options: [
-      '题目10/选项图片/video62_000604.jpg',
-      '题目10/选项图片/video62_000644.jpg',
-      '题目10/选项图片/video63_000276.jpg',
-      '题目10/选项图片/video63_000326.jpg',
-      '题目10/选项图片/video63_000359.jpg'
-    ]
-  },
-  {
-    id: 11,
-    queryImage: '题目11/题目图片/video63_000677.jpg',
-    options: [
-      '题目11/选项图片/video63_000697.jpg',
-      '题目11/选项图片/video63_000707.jpg',
-      '题目11/选项图片/video63_000724.jpg',
-      '题目11/选项图片/video66_000456.jpg',
-      '题目11/选项图片/video66_000509.jpg'
-    ]
-  },
-  {
-    id: 12,
-    queryImage: '题目12/题目图片/video67_000841.jpg',
-    options: [
-      '题目12/选项图片/video068_000154.jpg',
-      '题目12/选项图片/video67_000862.jpg',
-      '题目12/选项图片/video67_000887.jpg',
-      '题目12/选项图片/video67_001058.jpg',
-      '题目12/选项图片/video68_000829.jpg'
-    ]
-  },
-  {
-    id: 13,
-    queryImage: '题目13/题目图片/video68_000994.jpg',
-    options: [
-      '题目13/选项图片/video069_000090.jpg',
-      '题目13/选项图片/video069_000113.jpg',
-      '题目13/选项图片/video68_001014.jpg',
-      '题目13/选项图片/video69_000288.jpg',
-      '题目13/选项图片/video69_000369.jpg'
-    ]
-  },
-  {
-    id: 14,
-    queryImage: '题目14/题目图片/video71_000843.jpg',
-    options: [
-      '题目14/选项图片/video072_000227.jpg',
-      '题目14/选项图片/video072_000268.jpg',
-      '题目14/选项图片/video72_000551.jpg',
-      '题目14/选项图片/video73_000387.jpg',
-      '题目14/选项图片/video73_000531.jpg'
-    ]
-  },
-  {
-    id: 15,
-    queryImage: '题目15/题目图片/video086_000565.jpg',
-    options: [
-      '题目15/选项图片/video087_000122.jpg',
-      '题目15/选项图片/video088_000545.jpg',
-      '题目15/选项图片/video86_001010.jpg',
-      '题目15/选项图片/video87_000469.jpg',
-      '题目15/选项图片/video87_000634.jpg'
+      '题目10/选项图片/video58_000456.jpg',
+      '题目10/选项图片/video067_000496.jpg',
+      '题目10/选项图片/video86_000959.jpg',
+      '题目10/选项图片/video57_000405.jpg',
+      '题目10/选项图片/video87_000690.jpg',
     ]
   }
 ];
---
+++ questions_snippet.js
@@ -0,0 +1,112 @@
+const QUESTIONS = [
+  {
+    id: 1,
+    queryImage: '题目1/题目图片/video072_000102.jpg',
+    options: [
+      '题目1/选项图片/video87_000498.jpg',
+      '题目1/选项图片/video067_000210.jpg',
+      '题目1/选项图片/video072_000110.jpg',
+      '题目1/选项图片/video072_000119.jpg',
+      '题目1/选项图片/video072_000127.jpg',
+    ]
+  },
+  {
+    id: 2,
+    queryImage: '题目2/题目图片/video86_000989.jpg',
+    options: [
+      '题目2/选项图片/video86_000984.jpg',
+      '题目2/选项图片/video071_000295.jpg',
+      '题目2/选项图片/video86_001018.jpg',
+      '题目2/选项图片/video86_001017.jpg',
+      '题目2/选项图片/video86_001019.jpg',
+    ]
+  },
+  {
+    id: 3,
+    queryImage: '题目3/题目图片/video108_001386.jpg',
+    options: [
+      '题目3/选项图片/video57_000195.jpg',
+      '题目3/选项图片/video086_000429.jpg',
+      '题目3/选项图片/video59_000073.jpg',
+      '题目3/选项图片/video59_000096.jpg',
+      '题目3/选项图片/video59_000703.jpg',
+    ]
+  },
+  {
+    id: 4,
+    queryImage: '题目4/题目图片/video086_000610.jpg',
+    options: [
+      '题目4/选项图片/video088_000121.jpg',
+      '题目4/选项图片/video075_000539.jpg',
+      '题目4/选项图片/video58_000117.jpg',
+      '题目4/选项图片/video088_000176.jpg',
+      '题目4/选项图片/video58_000480.jpg',
+    ]
+  },
+  {
+    id: 5,
+    queryImage: '题目5/题目图片/video108_000842.jpg',
+    options: [
+      '题目5/选项图片/video108_000832.jpg',
+      '题目5/选项图片/video62_000238.jpg',
+      '题目5/选项图片/video108_000843.jpg',
+      '题目5/选项图片/video62_000350.jpg',
+      '题目5/选项图片/video26_000401.jpg',
+    ]
+  },
+  {
+    id: 6,
+    queryImage: '题目6/题目图片/video070_000858.jpg',
+    options: [
+      '题目6/选项图片/video070_000579.jpg',
+      '题目6/选项图片/video070_000818.jpg',
+      '题目6/选项图片/video70_001434.jpg',
+      '题目6/选项图片/video71_000689.jpg',
+      '题目6/选项图片/video66_000061.jpg',
+    ]
+  },
+  {
+    id: 7,
+    queryImage: '题目7/题目图片/video74_000404.jpg',
+    options: [
+      '题目7/选项图片/video70_001109.jpg',
+      '题目7/选项图片/video57_000554.jpg',
+      '题目7/选项图片/video74_000504.jpg',
+      '题目7/选项图片/video74_000388.jpg',
+      '题目7/选项图片/video74_000385.jpg',
+    ]
+  },
+  {
+    id: 8,
+    queryImage: '题目8/题目图片/video60_000639.jpg',
+    options: [
+      '题目8/选项图片/video58_000327.jpg',
+      '题目8/选项图片/video082_000171.jpg',
+      '题目8/选项图片/video60_000637.jpg',
+      '题目8/选项图片/video60_000638.jpg',
+      '题目8/选项图片/video60_000636.jpg',
+    ]
+  },
+  {
+    id: 9,
+    queryImage: '题目9/题目图片/video108_000239.jpg',
+    options: [
+      '题目9/选项图片/video108_000348.jpg',
+      '题目9/选项图片/video071_000151.jpg',
+      '题目9/选项图片/video108_000127.jpg',
+      '题目9/选项图片/video108_000189.jpg',
+      '题目9/选项图片/video26_000657.jpg',
+    ]
+  },
+  {
+    id: 10,
+    queryImage: '题目10/题目图片/video58_000480.jpg',
+    options: [
+      '题目10/选项图片/video58_000456.jpg',
+      '题目10/选项图片/video067_000496.jpg',
+      '题目10/选项图片/video86_000959.jpg',
+      '题目10/选项图片/video57_000405.jpg',
+      '题目10/选项图片/video87_000690.jpg',
+    ]
+  },
+];
